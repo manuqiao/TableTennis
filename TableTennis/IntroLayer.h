6192ce3b60546577a1e79e22bac50e352d444187
manuqiao <manuqiao@me.com>
2013-08-23 16:11:34 +0800

    initial commit
    加入了设备重力

---
+++ TableTennis/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  TableTennis
+//
+//  Created by qiaorujia on 8/23/13.
+//  Copyright qiaorujia 2013. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ TableTennis/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TableTennis
+//
+//  Created by qiaorujia on 8/23/13.
+//  Copyright qiaorujia 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
